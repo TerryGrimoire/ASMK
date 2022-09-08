7b2c86a2072d6e45439c67b0b28473c67306f2fd
Terry Grimoire <terry.grimoire@gmail.com>
2022-09-08 17:02:22 +0200

    adding lorem in fans page

--- src/pages/Fans.jsx
+++ src/pages/Fans.jsx
@@ -4,7 +4,40 @@ function Fans() {
   return (
     <main>
       <h1>Iels parlent de nous</h1>
-      <p>Patati Patata</p>
+      <article className="flex-col">
+        <h2>Patati Patata</h2>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae
+          quod rem at possimus rerum voluptates fugiat ea voluptatum. Impedit
+          cupiditate libero maxime ab corrupti, accusantium nihil. Debitis dicta
+          laboriosam accusantium saepe illum repellat incidunt perspiciatis ab
+          sint enim ipsa nesciunt cupiditate, porro aspernatur repellendus
+          facilis? Excepturi dolor exercitationem sapiente nulla.
+        </p>
+      </article>
+
+      <article className="flex-col">
+        <h2>Patati Patata</h2>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae
+          quod rem at possimus rerum voluptates fugiat ea voluptatum. Impedit
+          cupiditate libero maxime ab corrupti, accusantium nihil. Debitis dicta
+          laboriosam accusantium saepe illum repellat incidunt perspiciatis ab
+          sint enim ipsa nesciunt cupiditate, porro aspernatur repellendus
+          facilis? Excepturi dolor exercitationem sapiente nulla.
+        </p>
+      </article>
+      <article className="flex-col">
+        <h2>Patati Patata</h2>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae
+          quod rem at possimus rerum voluptates fugiat ea voluptatum. Impedit
+          cupiditate libero maxime ab corrupti, accusantium nihil. Debitis dicta
+          laboriosam accusantium saepe illum repellat incidunt perspiciatis ab
+          sint enim ipsa nesciunt cupiditate, porro aspernatur repellendus
+          facilis? Excepturi dolor exercitationem sapiente nulla.
+        </p>
+      </article>
     </main>
   );
 }
