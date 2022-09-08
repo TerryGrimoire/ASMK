d0b7ec65de232e46f36a00e7d17f130d40420f72
Terry Grimoire <terry.grimoire@gmail.com>
2022-09-08 12:04:03 +0200

    finishing the contact page and deleting the contact form but adding social media

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,41 +1,15 @@
-import React, { useState, useEffect } from "react";
-import Form from "../components/Form";
+import React, { useEffect } from "react";
+import SocialMedia from "../components/SocialMedia";
 
 function Contact() {
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
-  const [rdv, setRdv] = useState(true);
+
   return (
     <main className="flex flex-col align-center">
-      <h2>Nous joindre</h2>
-      <section>
-        <div className="flex justify-center margin1r0 ">
-          <button
-            type="button"
-            className="button-style margin0r1r"
-            onClick={() => setRdv(true)}
-          >
-            Prendre un rdv
-          </button>
-          <button
-            type="button"
-            className="button-style margin0r1r"
-            onClick={() => setRdv(false)}
-          >
-            Envoyer un email
-          </button>
-        </div>
-        {rdv ? (
-          <iframe
-            src="https://calendly.com/terry-grimoire/30min?month=2022-08"
-            frameBorder="0"
-            title="calendly terry grimoire"
-          />
-        ) : (
-          <Form />
-        )}
-      </section>
+      <h1>Nous joindre</h1>
+      <SocialMedia />
     </main>
   );
 }
